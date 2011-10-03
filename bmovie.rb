145937398624589a780fdbc7a0f107bc515e83ea
Ben Berry <hobershort@gmail.com>
2011-10-03 16:15:29 -0400

    Trying to mess with the config.ru file to get it to run my app

--- bmovie.rb
+++ bmovie.rb
@@ -1,43 +1,46 @@
+require 'rubygems'
+require 'sinatra'
 require 'net/http'
 require 'uri'
-require 'sinatra'
 
-fullsrc = Net::HTTP.get(URI.parse("http://www.gandronics.com/bmovie_dump.php"))
-#fullsrc = File.read("bmovie_dump.php")
-lines = fullsrc.split(/<[^>]*>/)
-lines = lines.select{|item| item =~ /\w*: .*/}
+#fullsrc = Net::HTTP.get(URI.parse("http://www.gandronics.com/bmovie_dump.php"))
+configure :production do
+	fullsrc = File.read("bmovie_dump.php")
+	lines = fullsrc.split(/<[^>]*>/)
+	lines = lines.select{|item| item =~ /\w*: .*/}
 
-item = lines.pop
-$intros = []
-$adjectives = [] 
-$modifiers = [] 
-$creatures = []
-$places = []
-$tags = []
+	item = lines.pop
+	$intros = []
+	$adjectives = [] 
+	$modifiers = [] 
+	$creatures = []
+	$places = []
+	$tags = []
 
-lines.each do |item|
-	case item[0,1]
-	when 'i'
-		m = item.match /intro: (.*)/
-		$intros << m[1]
-	when 'a'
-		m = item.match /adjective: (.*)/
-		$adjectives << m[1]
-	when 'm'
-		m = item.match /modifier: (.*)/
-		$modifiers << m[1]
-	when 'c'
-		m = item.match /creature: (.*)/
-		$creatures << m[1]
-	when 'p'
-		m = item.match /place: (.*)/
-		$places << m[1]
-	when 't'
-		m = item.match /tag: (.*)/
-		$tags << m[1]
-	else
-		if(item =~ /\S/)
-			puts "Line not matched: " + item
+	lines.each do |item|
+		case item[0,1]
+		when 'i'
+			m = item.match /intro: (.*)/
+			$intros << m[1]
+		when 'a'
+			m = item.match /adjective: (.*)/
+			$adjectives << m[1]
+		when 'm'
+			m = item.match /modifier: (.*)/
+			$modifiers << m[1]
+		when 'c'
+			m = item.match /creature: (.*)/
+			$creatures << m[1]
+		when 'p'
+			m = item.match /place: (.*)/
+			$places << m[1]
+		when 't'
+			m = item.match /tag: (.*)/
+			$tags << m[1]
+		else
+			if(item =~ /\S/)
+				puts "Line not matched: " + item
+			end
 		end
 	end
 end
